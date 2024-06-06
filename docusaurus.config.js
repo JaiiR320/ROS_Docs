c655db45e8d84750a8a86e6a5d9e94020604d826
JaiiR320 <jair3202@gmail.com>
2024-06-06 02:45:44 -0400

    fixed broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'ROS Help',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -88,7 +88,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/',
               },
             ],
           },
